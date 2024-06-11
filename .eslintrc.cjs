9f06db1746107d4738b8025e61677254eeabe51e
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2024-06-10 22:35:07 -0300

    chore: big bang

---
+++ .eslintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint-config-lesada'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+ 
+}
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Carol</h1>;
+}
+
+export default App;
