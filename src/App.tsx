2eecd995bcf17a874464fdee83f90450a950fc91
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2024-06-12 20:02:39 -0300

    feat: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 
 import Header from "./components/Header";
+import Routes from "./routes";
 
 import theme from "./styles";
 import GlobalStyle from "./styles/global";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Header />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Header />
+        <Routes />
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
