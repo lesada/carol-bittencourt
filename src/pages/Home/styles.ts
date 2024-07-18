afd07f99ecba5481074c66216b5665b280489dc3
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2024-07-18 10:18:48 -0300

    fix: fixed height

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -34,6 +34,10 @@ export const Row = styled.div`
 
 export const Video = styled.video`
   width: 100%;
-  height: 100%;
+  height: 8.625rem;
   object-fit: cover;
+
+  @media screen and (min-width: 768px) {
+    height: 20.75rem;
+  }
 `;
