5fc2785c3820375ebbb9aca2be3b73b66c900cd1
Lauren Leslie <68572174+lesada@users.noreply.github.com>
2025-03-26 18:40:57 -0300

    Feat: posts (#1)
    * feat: posts
    
    * fix: remove yarn
    
    * feat: color anchor

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -9,15 +9,13 @@ export type CardProps = {
   title: string;
   tags: string[];
   id: string;
-  image: string;
+  image: string | null;
 };
 
 function Card({ title, tags, image, id }: CardProps) {
   return (
     <Container to={PATHS.ARTICLE.replace(":id", id)}>
-      <Picture>
-        <img src={image} alt="" />
-      </Picture>
+      <Picture>{image && <img src={image} alt="" />}</Picture>
       <Wrapper>
         <Heading variant="x-small" as="h6" color={theme.colors.neutral[900]}>
           {title}
---
+++ src/components/DynamicContent/index.tsx
@@ -0,0 +1,86 @@
+import parse, {
+  DOMNode,
+  domToReact,
+  Element,
+  HTMLReactParserOptions,
+} from "html-react-parser";
+import Heading from "../Heading";
+import { Anchor, Container, Detach, Image, Legend, List, Text } from "./styles";
+
+type DynamicContentProps = {
+  content: string | null | undefined;
+};
+
+function DynamicContent({ content }: DynamicContentProps) {
+  if (!content || typeof content !== "string") return null;
+
+  const options: HTMLReactParserOptions = {
+    replace: (node: DOMNode) => {
+      if (node instanceof Element) {
+        const { name, attribs, children } = node;
+
+        const parsedChildren = domToReact(
+          children as unknown as DOMNode[],
+          options
+        );
+
+        switch (name) {
+          case "h2":
+            return (
+              <Heading as="h2" variant="small">
+                {parsedChildren}
+              </Heading>
+            );
+          case "h3":
+            return (
+              <Heading as="h3" variant="x-small">
+                {parsedChildren}
+              </Heading>
+            );
+          case "p":
+            return <Text>{parsedChildren}</Text>;
+          case "strong":
+            return (
+              <Text $bold as="span">
+                {parsedChildren}
+              </Text>
+            );
+          case "figcaption":
+            return <Legend>{parsedChildren}</Legend>;
+          case "blockquote":
+            return <Detach>{parsedChildren}</Detach>;
+          case "ul":
+            return <List>{parsedChildren}</List>;
+          case "li":
+            return <li>{parsedChildren}</li>;
+          case "br":
+            return <br />;
+          case "img":
+            return (
+              <Image>
+                <img src={attribs.src} alt={attribs.alt || ""} />
+                {attribs.alt && <Legend>{attribs.alt}</Legend>}
+              </Image>
+            );
+          case "figure":
+            return <Image>{parsedChildren}</Image>;
+          case "a":
+            return (
+              <Anchor href={attribs.href} rel="noopener noreferrer">
+                {parsedChildren}
+              </Anchor>
+            );
+          case "div":
+            return <div>{parsedChildren}</div>;
+          default:
+            return undefined;
+        }
+      }
+      return undefined;
+    },
+  };
+
+  return <Container>{parse(content, options)}</Container>;
+}
+
+export default DynamicContent;
